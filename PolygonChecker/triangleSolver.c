d85a1b4d27403bc0d38bd1398cab230ab7d2ddda
Abdelrahman Hashad <Ahashad3286@conestogac.on.ca>
2021-11-03 12:42:00 -0400

    Infinite loop fix when incorrect given incorrect input

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include "math.h"
 #include "triangleSolver.h"
 
 char* analyzeTriangle(int side1, int side2, int side3) {
 	char* result = "";
 	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
+		result = "Not a triangle\n";
 	}
-	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
+	else if (side1 == side2 && side1 == side3) 
+	{
+		result = "Equilateral triangle\n";
 	}
 	else if ((side1 == side2 && side1 != side3) || 
 		(side1 == side3 && side1 != side2))
 	{
-		result = "Isosceles triangle";
+		result = "Isosceles triangle\n";
 	}
-	else {
-		result = "Scalene triangle";
+	else 
+	{
+		result = "Scalene triangle\n";
 	}
 
 	return result;
