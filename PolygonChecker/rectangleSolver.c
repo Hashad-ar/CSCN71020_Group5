498f7cc407242a9a4380832c558359dae246669e
Abdelrahman Hashad <Ahashad3286@conestogac.on.ca>
2021-11-08 13:12:30 -0500

    Fixed rectangle files not Checked In Error

---
+++ PolygonChecker/rectangleSolver.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "math.h"
+#include "rectangleSolver.h"
+//added new main file for rectangle feature
+
+float* analyzeRectangle(int point1, int point2, int point3, int point4)
+{
+    float* result = "";
+    if (point1 < 0 || point2 < 0 || point3 < 0 || point4 < 0)
+    {
+        result = "Not a rectangle\n";
+    }
+    else if (point1)
+    {
+
+    }
+}
---
+++ PolygonChecker/rectangleSolver.h
@@ -0,0 +1,4 @@
+#pragma once
+//added new header file for rectangle input
+
+float* analyzeRectangle(int point1, int point2, int point3, int point4);
