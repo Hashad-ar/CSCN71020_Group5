17b6af0ac39b2d449d37c49a672519cd1a04463c
Joel Paul Fernandez <Jfernandez5735@conestogac.on.ca>
2021-11-16 18:41:08 -0500

    added 2 lines of code into main.h file and created unit test folder.

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -3,3 +3,5 @@ void printWelcome();
 int printShapeMenu();
 int* getTriangleSides(int* triangleSides);
 void getRectanglePoints(int* xAxis, int* yAxis);
+char* analyzeRectangle(double xaxis[4], double yaxis[4]);
+char* analyzeTriangle(int side1, int side2, int side3);
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern"C"
+{}
+#include "main.h"
+#include "traingleSolver.h"
+#include "rectangleSolver.h"
+}
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(Triangle_inside_angle1)
+		{
+			
+
+		}
+	};
+}
