e4736d90e51f5d4bf76cb64b55a4641398d941e1
amircana <amircan206@gmail.com>
2021-11-25 16:16:34 -0500

    fixed an issue where the triangle inequality theorem included 2 sides being equal to the third

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -8,7 +8,7 @@
 char* analyzeTriangle(int side1, int side2, int side3) 
 {
 	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 < side3 || side2 + side3 < side1 || side3 + side1 < side2) // added triangle inequality theorem
+	if (side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) // added triangle inequality theorem
 	{
 		result = "Not a triangle\n";
 	}
