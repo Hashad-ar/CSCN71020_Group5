626e91e12eb8e231c7a5231100cef67bd81f77e6
amircana <amircan206@gmail.com>
2021-11-28 16:22:15 -0500

    tweaked the 3 test functions with the triangle to work with the new function for the angle, needs checking and if works then delete the commented versions

--- UnitTest1/UnitTest1.cpp
+++ UnitTest1/UnitTest1.cpp
@@ -50,6 +50,15 @@ namespace UnitTest1
 			char* result = "Not a triangle\n";
 			Assert::AreEqual(result, analyzeTriangle(side1, side2, side3));
 		}
+		//Testing if the function will output the correct result for invalid input that's not a triangle using -3, 0, 3 for side lengths.
+		TEST_METHOD(TypeOfTriangle_NotATriangleLetterInput)
+		{
+			int side1 = 'a';
+			int side2 = 0;
+			int side3 = 3;
+			char* result = "Not a triangle\n";
+			Assert::AreEqual(result, analyzeTriangle(side1, side2, side3));
+		}
 	};
 
 	TEST_CLASS(AnglesInsideTriangle)
@@ -58,12 +67,13 @@ namespace UnitTest1
 		//Testing if the function will output the correct result for the angles inside an Equilateral triangle with expected result as 60 for all side lengths.
 		TEST_METHOD(AnglesInsideTriangle_Equilateral)
 		{
+			/*
 			int side1 = 3;
 			int side2 = 3;
 			int side3 = 3;
 			float result = 60;
 
-			/*The angle calculator function copied and pasted*/
+			The angle calculator function copied and pasted
 			float temp1 = (float)(side1 * side1 + side2 * side2 - (side3 * side3)) / (2 * side1 * side2);
 			float temp2 = (float)(side2 * side2 + side3 * side3 - side1 * side1) / (2 * side2 * side3);
 			float angle1 = acos(temp1) * (180 / 3.14159265358979323846); // converting radians to angles 
@@ -73,10 +83,23 @@ namespace UnitTest1
 			Assert::AreEqual(result, angle1);
 			Assert::AreEqual(result, angle2);
 			Assert::AreEqual(result, angle3);
+			*/
+
+			int side1 = 3;
+			int side2 = 3;
+			int side3 = 3;
+			float result = 60; // the desired result 
+
+			float * FuncResult = analyzeAngle(side1, side2, side3);
+
+			Assert::AreEqual(result, FuncResult[0]);
+			Assert::AreEqual(result, FuncResult[1]);
+			Assert::AreEqual(result, FuncResult[2]);
 		}
 		//Testing if the function will output the correct result for the angles of an Isoceles triangle with expected result of approximately  39, 70.5, 70.5. 
 		TEST_METHOD(AnglesInsideTriangle_Isosceles)
 		{
+			/*
 			int side1 = 2;
 			int side2 = 3;
 			int side3 = 3;
@@ -84,7 +107,6 @@ namespace UnitTest1
 			float result2 = 38.942441268981;
 			float result3 = 70.528779365509;
 
-			/*The angle calculator function copied and pasted*/
 			float temp1 = (float)(side1 * side1 + side2 * side2 - (side3 * side3)) / (2 * side1 * side2);
 			float temp2 = (float)(side2 * side2 + side3 * side3 - side1 * side1) / (2 * side2 * side3);
 			float angle1 = acos(temp1) * (180 / 3.14159265358979323846); // converting radians to angles 
@@ -94,27 +116,78 @@ namespace UnitTest1
 			Assert::AreEqual(result1, angle1);
 			Assert::AreEqual(result2, angle2);
 			Assert::AreEqual(result3, angle3);
+			*/
+
+			int side1 = 2;
+			int side2 = 3;
+			int side3 = 3;
+			float result1 = 70.528779365509; //desired results 
+			float result2 = 38.942441268981;
+			float result3 = 70.528779365509;
+			float* FuncResult = analyzeAngle(side1, side2, side3);
+
+			Assert::AreEqual(result1, FuncResult[0]);
+			Assert::AreEqual(result2, FuncResult[1]);
+			Assert::AreEqual(result3, FuncResult[2]);
+
 		}
 		//Testing if the function will output the correct result for the angles of an Scalene triangle with expected result of approximately  90, 36.9, 53.1.
 		TEST_METHOD(AnglesInsideTriangle_Scalene)
 		{
-			int side1 = 3;
+			/*int side1 = 3;
 			int side2 = 4;
 			int side3 = 5;
 			float result1 = 90;
 			float result2 = 36.86989764584;
 			float result3 = 53.130102354156;
 
-			/*The angle calculator function copied and pasted*/
-			float temp1 = (float)(side1 * side1 + side2 * side2 - (side3 * side3)) / (2 * side1 * side2);
-			float temp2 = (float)(side2 * side2 + side3 * side3 - side1 * side1) / (2 * side2 * side3);
-			float angle1 = acos(temp1) * (180 / 3.14159265358979323846); // converting radians to angles 
-			float angle2 = acos(temp2) * (180 / 3.14159265358979323846); // converting radians to angles 
-			float angle3 = 180 - angle1 - angle2;
+			float* arrr[3] = analyzeAngle(side1, side2, side3);
+			
 
 			Assert::AreEqual(result1, angle1);
 			Assert::AreEqual(result2, angle2);
 			Assert::AreEqual(result3, angle3);
+			*/
+			int side1 = 3;
+			int side2 = 4;
+			int side3 = 5;
+			float result1 = 90; // desired results 
+			float result2 = 36.86989764584;
+			float result3 = 53.130102354156;
+			float* FuncResult = analyzeAngle(side1, side2, side3);
+
+			Assert::AreEqual(result1, FuncResult[0]);
+			Assert::AreEqual(result2, FuncResult[1]);
+			Assert::AreEqual(result3, FuncResult[2]);
+		}
+
+		TEST_METHOD(AnglesInsideTriangle_Scalene)
+		{
+			/*int side1 = 3;
+			int side2 = 4;
+			int side3 = 5;
+			float result1 = 90;
+			float result2 = 36.86989764584;
+			float result3 = 53.130102354156;
+
+			float* arrr[3] = analyzeAngle(side1, side2, side3);
+
+
+			Assert::AreEqual(result1, angle1);
+			Assert::AreEqual(result2, angle2);
+			Assert::AreEqual(result3, angle3);
+			*/
+			int side1 = 3;
+			int side2 = 4;
+			int side3 = 5;
+			float result1 = 90; // desired results 
+			float result2 = 36.86989764584;
+			float result3 = 53.130102354156;
+			float* FuncResult = analyzeAngle(side1, side2, side3);
+
+			Assert::AreEqual(result1, FuncResult[0]);
+			Assert::AreEqual(result2, FuncResult[1]);
+			Assert::AreEqual(result3, FuncResult[2]);
 		}
 	};
 
